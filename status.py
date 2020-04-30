54157543c04994545e2893195f6cd4be5d55a371
JaimeADalton <jaimeadalton@gmail.com>
2020-04-30 20:45:05 +0200

    Update status.py

--- status.py
+++ status.py
@@ -2,10 +2,16 @@
 import time
 import os
 
+#Esta funcion verifica el estado del servicio mysql: 0 OK; otro_numero Error
 def status():
+        max_time = 60
+        start_time = time.time()
         status = os.system('systemctl is-active --quiet mysql')
         while status != 0:
                 time.sleep(30)
                 status = os.system('systemctl is-active --quiet mysql')
+                end_time = time.time()
+                if end_time - start_time > max_time :
+                        return False
         else:
             return True
